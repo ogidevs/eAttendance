d641d4d15f6b5d7da05ba731e65249f088c0b2c6
ognjen <milosevicognjen03@gmail.com>
2025-06-23 11:56:17 +0200

    first commit

---
+++ src/main/java/com/ognjen/eattendance/EattendanceApplication.java
@@ -0,0 +1,13 @@
+package com.ognjen.eattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EattendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EattendanceApplication.class, args);
+	}
+
+}
